aa47cc5b3a5294892b53622eae9f2fbd93842f32
jzallen <zallen@alightanalytics.com>
2021-11-17 03:14:39 +0000

    not simple react

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
         <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Simple React App</h1>
+          <h1 className="App-title">React App</h1>
         </header>
           <Switch>
                 <Route exact path= "/" render={() => (
