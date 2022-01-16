f28c78b483ba056d2aea4a74667b7fd308d73345
David Pinhas <davepinhas89@gmail.com>
2022-01-16 16:17:21 +0200

    updated artifactory-config.sh script

--- artifactory-config.sh
+++ artifactory-config.sh
@@ -1,6 +1,7 @@
-if  grep $int_pegeneric_server ~/.jfrog/jfrog-cli.conf.v5;
+if  grep "$int_pegeneric_server 'serverId': '$int_pegeneric_server'" ~/.jfrog/jfrog-cli.conf.v5;
 then 
-  jfrog config use $int_pegeneric_server
+  jfrog config use $int_pegeneric_server;
 else 
-  jfrog config add $int_pegeneric_server --artifactory-url https://$int_pegeneric_server.jfrog.io/artifactory --user $int_pegeneric_user --password $int_pegeneric_password --interactive=false; 
+  jfrog config add $int_pegeneric_server --artifactory-url https://$int_pegeneric_server.jfrog.io/artifactory --user $int_pegeneric_user --password $int_pegeneric_password --interactive=false
+  jfrog config use $int_pegeneric_server;
 fi
