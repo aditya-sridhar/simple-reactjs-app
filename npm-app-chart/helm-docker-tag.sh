d61ef5cff80e76ac0525d7f38a35b4f9d7b5239a
David Pinhas <davepinhas89@gmail.com>
2022-01-19 09:37:59 +0200

    removed the docker tag convention res_pe_source_code_commitSha from dockerImageTagVersion

--- npm-app-chart/helm-docker-tag.sh
+++ npm-app-chart/helm-docker-tag.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find values.yaml -type f -exec sed -i -e "s|tag:|tag: ${dockerImageTagVersion}${res_pe_source_code_commitSha}|g" {} \;
+find values.yaml -type f -exec sed -i -e "s|tag:|tag: ${dockerImageTagVersion}|g" {} \;
