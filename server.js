dcb2507a3a76daef384e3dd63248106563aad39b
David Pinhas <davepinhas89@gmail.com>
2022-01-24 14:08:56 +0200

    test commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World1');
+  res.send('Hello World');
 });
 app.get('/health-check',(req,res)=> {
   res.send ("Health check passed");
